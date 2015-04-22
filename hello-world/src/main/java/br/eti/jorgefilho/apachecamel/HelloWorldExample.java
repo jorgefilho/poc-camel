50d9383c1f2f1835d9c50a55342792215cc041ca
jorgefilho <papachristo@gmail.com>
2015-04-22 06:28:55 -0300

    second commit

---
+++ hello-world/src/main/java/br/eti/jorgefilho/apachecamel/HelloWorldExample.java
@@ -0,0 +1,21 @@
+package br.eti.jorgefilho.apachecamel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldExample 
+{
+   public static void main(String[] args) throws Exception{
+	   CamelContext context = new DefaultCamelContext();
+	   try {
+		
+	   } catch (Exception e) {
+			// TODO: handle exception
+	   }
+	   
+   }
+}
