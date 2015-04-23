0655851327ec68ed321cd968c0b78d964b8c9a21
jorgefilho <papachristo@gmail.com>
2015-04-22 21:30:32 -0300

    Finishing the first example.

--- hello-world/src/main/java/br/eti/jorgefilho/apachecamel/HelloWorldExample.java
+++ hello-world/src/main/java/br/eti/jorgefilho/apachecamel/HelloWorldExample.java
@@ -1,6 +1,9 @@
 package br.eti.jorgefilho.apachecamel;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
@@ -12,9 +15,21 @@ public class HelloWorldExample
    public static void main(String[] args) throws Exception{
 	   CamelContext context = new DefaultCamelContext();
 	   try {
-		
-	   } catch (Exception e) {
-			// TODO: handle exception
+		   context.addComponent("activemq", ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false"));
+		   context.addRoutes(new RouteBuilder() {
+			   @Override
+			   public void configure () throws Exception{
+				   from("activemq:queue:test.queue")
+				   .to("stream:out");
+			   }
+		   });
+		   
+		   ProducerTemplate template = context.createProducerTemplate();
+		   context.start();
+		   template.sendBody("activemq:test.queue", "Hello World");
+		   Thread.sleep(2000);
+	   } finally {
+			context.stop();
 	   }
 	   
    }
